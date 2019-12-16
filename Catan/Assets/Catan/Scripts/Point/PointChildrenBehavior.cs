cbe8ae9bb175d54b414f213c9913d915276203b7
tanaka_toilet <donkiba77@gmail.com>
2019-12-16 14:47:46 +0900

    追加

---
+++ Catan/Assets/Catan/Scripts/Point/PointChildrenBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Catan.Scripts.Point
+{
+   // [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    [System.Serializable]
+    public class PointChildrenBehavior : MonoBehaviour
+    {
+        [SerializeField] public int pointNumber;
+        [SerializeField] GameObject poimtObject;
+        [SerializeField] GameObject AdjacentPoint_0;
+        [SerializeField] GameObject AdjacentPoint_1;
+        [SerializeField] GameObject AdjacentPoint_2;
+
+    }
+
+}
---
+++ Catan/Assets/Catan/Scripts/Point/PointParentBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Catan.Scripts.Point
+{
+   // [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    [System.Serializable]
+    public class PointParentBehavior : MonoBehaviour
+    {
+        [SerializeField] public int pointNumber;
+        [SerializeField] GameObject poimtObject;
+        [SerializeField] GameObject AdjacentPoint_0;
+        [SerializeField] GameObject AdjacentPoint_1;
+        [SerializeField] GameObject AdjacentPoint_2;
+        [SerializeField] GameObject AdjacentPoint_3;
+        [SerializeField] GameObject AdjacentPoint_4;
+        [SerializeField] GameObject AdjacentPoint_5;
+
+
+    }
+
+}
