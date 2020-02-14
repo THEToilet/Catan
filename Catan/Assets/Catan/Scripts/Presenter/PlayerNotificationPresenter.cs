90884a51190cfeec843c9aad49e19279b80dc5f6
TheToilet <bp18015@shibaura-it.ac.jp>
2020-02-15 02:32:05 +0900

    クラス図新規作成

--- Catan/Assets/Catan/Scripts/Presenter/PlayerNotificationPresenter.cs
+++ Catan/Assets/Catan/Scripts/Presenter/PlayerNotificationPresenter.cs
@@ -15,6 +15,8 @@ namespace Catan.Scripts.Presenter
     public ReactiveProperty<PlayerId> _currentPlayerId;
     public PlayerTurn playerTurn;
 
+    public Text playerText;
+
         void Update()
         {
             //  Debug.Log(playerTurn._currentPlayerId.Value);
@@ -39,25 +41,6 @@ namespace Catan.Scripts.Presenter
             }
         }
 
-    void Update()
-    {
-      //  Debug.Log(playerTurn._currentPlayerId.Value);
-      switch (playerTurn._currentPlayerId.Value)
-      {
-        case (PlayerId.Player1):
-          playerText.text = "Player1";
-          break;
-        case (PlayerId.Player2):
-          playerText.text = "Player2";
-          break;
-        case (PlayerId.Player3):
-          playerText.text = "Player3";
-          break;
-        case (PlayerId.Player4):
-          playerText.text = "Player4";
-          break;
-      }
-    }
 
   }
 
