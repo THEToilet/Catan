cf27d437656c2e6dd58226a2352d1935ac2b0d69
TheToilet <bp18015@shibaura-it.ac.jp>
2020-06-20 22:19:32 +0900

    :sparkles: NumberToken 生成

--- Catan/Assets/Catan/Scripts/Generation/TerrainNumberGeneration.cs
+++ Catan/Assets/Catan/Scripts/Generation/TerrainNumberGeneration.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 using Catan.Scripts.Terrain;
 using Catan.Scripts.Common;
 using Catan.Scripts.Point;
+using ModestTree;
 
 namespace Catan.Scripts.Generation
 {
     public class TerrainNumberGeneration : MonoBehaviour
     {
         public PointParentGeneration pointParentGeneration;
+        [SerializeField] GameObject numberToken;
         private int[] NumberToken;
         public void Distribution()
         {
             NumberToken = Dice.NumberToken();
+            GameObject tmp;
             for (int i = 0; i < NumberToken.Length; i++)
-{
+            {
+                var p = pointParentGeneration.parentPointObjects[i];
                 Debug.Log(NumberToken[i]);
-         //   Debug.Log( pointParentGeneration.parentPointObjects[i].GetComponent<PointParentBehavior>().tokenNumber);
-               pointParentGeneration.parentPointObjects[i].GetComponent<PointParentBehavior>().tokenNumber = NumberToken[i];  // TerrainGameObjectのBehaviorに数字トークンを代入する
+                p.GetComponent<PointParentBehavior>().tokenNumber = NumberToken[i];  // TerrainGameObjectのBehaviorに数字トークンを代入する
+                tmp = GameObject.Instantiate(numberToken, new Vector3(p.transform.position.x, p.transform.position.y + 5f, p.transform.position.z), Quaternion.Euler(90, -90, 0)) ;
+                tmp.GetComponent<TextMesh>().text = NumberToken[i].ToString();
             }
         }
     }
