a71290651b1e7fe73f60accff82d0296228382a0
TheToilet <bp18015@shibaura-it.ac.jp>
2020-06-22 03:01:28 +0900

    Currentplayer のUIを追加

---
+++ Catan/Assets/Catan/Scripts/Presenter/CurrentPlayerPresenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Catan.Scripts.Player;
+using Catan.Scripts.Manager;
+using UnityEngine.UI;
+
+namespace Catan.Scripts.Presenter
+{
+    public class CurrentPlayerPresenter : MonoBehaviour
+    {
+        public PlayerTurnManeger playerTurnManeger;
+        [SerializeField] Text currentPlayer;
+        void Update()
+        {
+                currentPlayer.text = playerTurnManeger._currentPlayerId.ToString();
+
+        }
+    }
+
+}
