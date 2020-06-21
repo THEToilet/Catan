66f2c6ece1c343a0d8e7aecf90b06cf266b0d07b
TheToilet <bp18015@shibaura-it.ac.jp>
2020-06-22 01:54:42 +0900

    PlaterInfo UI を追加

---
+++ Catan/Assets/Catan/Scripts/Presenter/PlayerInfoPresenter.cs
@@ -0,0 +1,37 @@
+﻿using Catan.Scripts.Manager;
+using Catan.Scripts.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEngine.UI;
+
+public class PlayerInfoPresenter : MonoBehaviour
+{
+    public ToPleyerObject toPleyerObject;
+    public ProgressStateManeger progressStateManeger;
+    public PlayerTurnManeger playerTurnManeger;
+    [SerializeField] List<Text> player = new List<Text>(4);
+    [SerializeField] List<Text> point = new List<Text>(4);
+    [SerializeField] List<Text> card = new List<Text>(4);
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (playerTurnManeger._currentTurnState.Value == TurnState.RollDice)
+        {
+            var p = playerTurnManeger.playerIds;
+            for(int i = 0;i< p.Length; i++)
+            {
+                point[i].text = (toPleyerObject.ToPlayer(p[i]).GetComponent<PlayerCore>().playerScore.Value).ToString(); 
+                card[i].text = (toPleyerObject.ToPlayer(p[i]).GetComponent<Belongings>().cards.Count).ToString(); 
+                player[i].text = toPleyerObject.ToPlayer(p[i]).name; 
+            }
+
+        }
+
+    }
+}
