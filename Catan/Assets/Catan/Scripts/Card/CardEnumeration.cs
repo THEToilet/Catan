b85b99d9dceeabcdf4c7d3517d824839d98149a3
TheToilet <bp18015@shibaura-it.ac.jp>
2020-06-22 21:42:33 +0900

    カードの枚数を数えるクラス作成

--- Catan/Assets/Catan/Scripts/Card/CardEnumeration.cs
+++ Catan/Assets/Catan/Scripts/Card/CardEnumeration.cs
@@ -9,9 +9,34 @@ namespace Catan.Scripts.Card
     public class CardEnumeration : MonoBehaviour
     {
         public ToPleyerObject toPleyerObject;
-        public  void Enumeration(PlayerId playerId)
+        public int[] Enumeration(PlayerId playerId)
         {
-            var p = toPleyerObject.ToPlayer(playerId);
+            var p = toPleyerObject.ToPlayer(playerId).GetComponent<Belongings>();
+            int[] num = new int[] { 0, 0, 0, 0, 0 };
+            for(int i = 0; i < p.cards.Count; i++)
+            {
+                switch (p.cards[i].GetComponent<CardEntity>().cardType)
+                {
+                    case (CardType.Brick):
+                        num[0]++;
+                        break;
+                    case (CardType.IronOre):
+                        num[1]++;
+                        break;
+                    case (CardType.Wheat):
+                        num[2]++;
+                        break;
+                    case (CardType.Wood):
+                        num[3]++;
+                        break;
+                    case (CardType.Wool):
+                        num[4]++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return num;
                         
 
         }
