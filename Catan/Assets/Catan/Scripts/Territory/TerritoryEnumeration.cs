8d7b271cb7cff6f1ae9a40a7df2cecae3b4c573c
TheToilet <bp18015@shibaura-it.ac.jp>
2020-06-22 22:03:18 +0900

    陣地の数を数えるクラス作成

--- Catan/Assets/Catan/Scripts/Territory/TerritoryEnumeration.cs
+++ Catan/Assets/Catan/Scripts/Territory/TerritoryEnumeration.cs
@@ -14,15 +14,27 @@ namespace Catan.Scripts.Territory
         public int[] Enmeration(PlayerId playerId)
         {
             int[] num = new int[] { 0, 0, 0 };
-            var p = toPleyerObject.ToPlayer(playerId).GetComponent<Belongings>(); 
-            
-            /*
-            for(int i = 0; i < p.City.Count; i++)
+            var p = toPleyerObject.ToPlayer(playerId).GetComponent<Belongings>();
+
+            for (int i = 0; i < p.City.Count; i++)
             {
-                switch(p.City[i].)
+                switch (p.City[i].GetComponent<TerritoryEntity>().territoryType)
+                {
+
+                    case (TerritoryType.City):
+                        num[0]++;
+                        break;
+                    case (TerritoryType.Road):
+                        num[1]++;
+                        break;
+                    case (TerritoryType.Settlement):
+                        break;
+                    default:
+                        break;
+                }
+
             }
 
-*/
             return num;
 
 
