94dde076f4e94f2eb49fac7c3253d378471dda6e
TheToilet <bp18015@shibaura-it.ac.jp>
2020-06-22 20:59:06 +0900

    陣地のプレハブ変更

---
+++ Catan/Assets/Catan/Scripts/Card/CardEnumeration.cs
@@ -0,0 +1,21 @@
+﻿using Catan.Scripts.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Catan.Scripts.Card
+{
+
+    public class CardEnumeration : MonoBehaviour
+    {
+        public ToPleyerObject toPleyerObject;
+        public  void Enumeration(PlayerId playerId)
+        {
+            var p = toPleyerObject.ToPlayer(playerId);
+                        
+
+        }
+        
+    }
+
+}
---
+++ Catan/Assets/Catan/Scripts/Territory/TerritoryEnumeration.cs
@@ -0,0 +1,34 @@
+﻿using Catan.Scripts.Player;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace Catan.Scripts.Territory
+{
+
+    public class TerritoryEnumeration : MonoBehaviour
+    {
+        public ToPleyerObject toPleyerObject;
+
+        public int[] Enmeration(PlayerId playerId)
+        {
+            int[] num = new int[] { 0, 0, 0 };
+            var p = toPleyerObject.ToPlayer(playerId).GetComponent<Belongings>(); 
+            
+            /*
+            for(int i = 0; i < p.City.Count; i++)
+            {
+                switch(p.City[i].)
+            }
+
+*/
+            return num;
+
+
+        }
+
+    }
+
+
+}
