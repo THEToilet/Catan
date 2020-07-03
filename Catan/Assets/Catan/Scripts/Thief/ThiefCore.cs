083bfdea1e466db3ca5ec63482ef4ca2f0091bf1
TheToilet <bp18015@shibaura-it.ac.jp>
2020-07-04 03:31:03 +0900

    Theif インスタンス実装

---
+++ Catan/Assets/Catan/Scripts/Thief/ThiefCore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Catan.Scripts.Theif
+{
+
+    public class ThiefCore : MonoBehaviour
+    {
+        public GameObject theifPosition;
+    }
+
+}
