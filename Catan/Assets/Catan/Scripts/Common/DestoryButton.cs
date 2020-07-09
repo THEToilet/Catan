269a61a0f5850c60bcba55b3897a11185d4b5c1d
TheToilet <bp18015@shibaura-it.ac.jp>
2020-07-09 17:47:04 +0900

    Button押下じにDestory

---
+++ Catan/Assets/Catan/Scripts/Common/DestoryButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Catan.Scripts.Common
+{
+
+    public class DestoryButton : MonoBehaviour
+    {
+
+        Button btn;
+
+        private void Start()
+        {
+            btn = GetComponent<Button>();
+        }
+
+        public void OnClick()
+        {
+            GameObject.Destroy(this.btn.gameObject);
+        }
+
+    }
+
+}
