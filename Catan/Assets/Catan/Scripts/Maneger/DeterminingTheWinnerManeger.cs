f92f0192656be2d4e6067a9fa7f1a519fea86ecc
TheToilet <bp18015@shibaura-it.ac.jp>
2020-07-10 22:16:04 +0900

    VictoryPointが10以上になった時の実装

---
+++ Catan/Assets/Catan/Scripts/Maneger/DeterminingTheWinnerManeger.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Catan.Scripts.Player;
+using UniRx;
+using Catan.Scripts.Presenter;
+using System.Linq;
+
+namespace Catan.Scripts.Manager
+{
+    public struct playerI
+    {
+        public int point;
+        public PlayerId p;
+    }
+
+    public class DeterminingTheWinnerManeger : MonoBehaviour
+    {
+        public PlayerTurnManeger playerTurnManeger;
+        public ProgressStateManeger progressStateManeger;
+        public ToPleyerObject toPleyerObject;
+        public GameObject winner;
+        public GameResultPresenter gameResultPresenter;
+
+        private PlayerId[] playerIds = { PlayerId.Player1, PlayerId.Player2, PlayerId.Player3, PlayerId.Player4 };
+
+        List<playerI> pI = new List<playerI>();
+
+        public void Start()
+        {
+            // TODO このクラスでやりたかったことをRxでやる
+            /*  winnerScore.ObserveEveryValueChanged(_ => _.Value)
+                    .Where(x => x >= 10)
+                    .Subscribe(_ =>
+                        gameResultPresenter.Show())
+                    );*/
+        }
+
+        private void Update()  // 優勝者がいるまでまわす
+        {
+            if (playerTurnManeger._currentTurnState.Value == TurnState.NormalTurn)
+            {
+
+                for (int i = 0; i < playerIds.Length; i++)
+                {
+                    var p = toPleyerObject.ToPlayer(playerIds[i]);
+                    // ここでplayerTurnをそれぞれのプレイヤーのスコアが10以上になるまで回す
+                    if (p.GetComponent<PlayerCore>().playerScore.Value >= 10)
+                    {
+                        winner = p;
+                        for (int j = 0; j < playerIds.Length; j++)
+                        {
+                            playerI s = new playerI() { point = p.GetComponent<PlayerCore>().playerScore.Value, p = playerIds[j] };
+                            pI.Add(s);
+                        }
+                        IOrderedEnumerable<playerI> player = pI.OrderByDescending(rec => rec.point);
+                        gameResultPresenter.ShowGameResult(player);
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ Catan/Assets/Catan/Scripts/Presenter/GameResultPresenter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Catan.Scripts.Manager;
+using System.Linq;
+using DG.Tweening;
+namespace Catan.Scripts.Presenter
+{
+
+    public class GameResultPresenter : MonoBehaviour
+    {
+        [SerializeField] GameObject resultBoard;
+        [SerializeField] List<Text> pointText = new List<Text>(4);
+        [SerializeField] List<Text> playerText = new List<Text>(4);
+
+        public void ShowGameResult(IOrderedEnumerable<playerI> list)
+        {
+            int index = 0;
+            foreach (playerI rec in list)
+            {
+                pointText[index].text = rec.point.ToString();
+                playerText[index].text = rec.p.ToString();
+                index++;
+            }
+            resultBoard.transform.DOMove(new Vector3(0, -300f, 0), 5.0f);
+
+        }
+    }
+
+}
