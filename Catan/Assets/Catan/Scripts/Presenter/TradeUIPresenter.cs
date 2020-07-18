36112c7b6b30004131f2118f8d15679023945881
TheToilet <bp18015@shibaura-it.ac.jp>
2020-07-19 03:39:43 +0900

    Trade の　UI作成

--- Catan/Assets/Catan/Scripts/Presenter/TradeUIPresenter.cs
+++ Catan/Assets/Catan/Scripts/Presenter/TradeUIPresenter.cs
@@ -24,6 +24,10 @@ namespace Catan.Scripts.Presenter
         [SerializeField] Button wheat2Button;
         [SerializeField] Button ironOre2Button;
         [SerializeField] Button brick2Button;
+        [SerializeField] Button tradeButton;
+        [SerializeField] Button tradeCancelButton;
+        [SerializeField] GameObject tradePanel;
+        [SerializeField] GameObject deleteSelectPanel;
         public PlayerTurnManeger playerTurnManeger;
         public ToPleyerObject toPleyerObject;
         public CardEnumeration cardEnumeration;
@@ -39,60 +43,56 @@ namespace Catan.Scripts.Presenter
         int[] trade3 = new int[] { 15, 16, 45, 46, 18, 39, 52, 53 };
         [SerializeField] GameObject selectKindPanel;
 
-        public void TurnOffAll()
-        {
-            wood2Button.interactable = false;
-            wool2Button.interactable = false;
-            wheat2Button.interactable = false;
-            ironOre2Button.interactable = false;
-            brick2Button.interactable = false;
-            wood3Button.interactable = false;
-            wool3Button.interactable = false;
-            wheat3Button.interactable = false;
-            ironOre3Button.interactable = false;
-            brick3Button.interactable = false;
-        }
         void Start()
         {
+            tradeButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                tradePanel.SetActive(true);
+            });
+            tradeCancelButton.OnClickAsObservable().Subscribe(_ =>
+            {
+                tradePanel.SetActive(false);
+            });
+
             wood2Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Forest));
                 deleteResourcePresenter.numberOfCards = 2;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             wool2Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Pasture));
                 deleteResourcePresenter.numberOfCards = 2;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             wheat2Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Hill));
                 deleteResourcePresenter.numberOfCards = 2;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             ironOre2Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Mine));
                 deleteResourcePresenter.numberOfCards = 2;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             brick2Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Field));
                 deleteResourcePresenter.numberOfCards = 2;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
 
 
@@ -101,40 +101,40 @@ namespace Catan.Scripts.Presenter
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Forest));
                 deleteResourcePresenter.numberOfCards = 3;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             wool3Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Pasture));
                 deleteResourcePresenter.numberOfCards = 3;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             wheat3Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Hill));
                 deleteResourcePresenter.numberOfCards = 3;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             ironOre3Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Mine));
                 deleteResourcePresenter.numberOfCards = 3;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
             brick3Button.OnClickAsObservable().Subscribe(_ =>
             {
                 var p = toPleyerObject.ToPlayer(playerTurnManeger._currentPlayerId.Value).GetComponent<Belongings>().cards;
                 p.Add(toCardObject.ToCard(Terrain.TerrainType.Field));
                 deleteResourcePresenter.numberOfCards = 3;
-                this.TurnOffAll();
-                selectKindPanel.SetActive(true);
+                tradePanel.SetActive(false);
+                deleteSelectPanel.SetActive(true);
             });
 
         }
@@ -169,21 +169,41 @@ namespace Catan.Scripts.Presenter
                     {
                         brick2Button.interactable = true;
                     }
+                    else
+                    {
+                        brick2Button.interactable = false;
+                    }
                     if (CheckLocate(io2, c))
                     {
                         ironOre2Button.interactable = true;
+                    }
+                    else
+                    {
+                        ironOre2Button.interactable = false;
                     }
                     if (CheckLocate(wt2, c))
                     {
                         wheat2Button.interactable = true;
                     }
+                    else
+                    {
+                        wheat2Button.interactable = false;
+                    }
                     if (CheckLocate(wd2, c))
                     {
                         wood2Button.interactable = true;
                     }
+                    else
+                    {
+                        wood2Button.interactable = false;
+                    }
                     if (CheckLocate(wl2, c))
                     {
                         wool2Button.interactable = true;
+                    }
+                    else
+                    {
+                        wool2Button.interactable = false;
                     }
                 }
                 if ((num[0] >= 3 || num[1] >= 3 || num[2] >= 3 || num[3] >= 3 || num[4] >= 3) && (CheckLocate(trade3, c)))
@@ -193,6 +213,15 @@ namespace Catan.Scripts.Presenter
                     wheat3Button.interactable = true;
                     wood3Button.interactable = true;
                     wool3Button.interactable = true;
+                }
+
+                else
+                {
+                    brick3Button.interactable = false;
+                    ironOre3Button.interactable = false;
+                    wheat3Button.interactable = false;
+                    wood3Button.interactable = false;
+                    wool3Button.interactable = false;
                 }
             }
 
