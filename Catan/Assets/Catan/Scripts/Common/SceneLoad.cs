a978e3033d5b9c0eaac8480e31af8c2ffe04841b
TheToilet <bp18015@shibaura-it.ac.jp>
2020-07-21 13:04:26 +0900

    見た目改良

---
+++ Catan/Assets/Catan/Scripts/Common/SceneLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Catan.Scripts.Common
+{
+
+    public class SceneLoad : MonoBehaviour
+    {
+        public void ToMain()
+        {
+            SceneManager.LoadScene("Main");
+        }
+        public void ToTitle()
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+
+
+}
